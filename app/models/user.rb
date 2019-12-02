fbed6e027a03fc07442d23a2026827b11a4926e8
Vedant <vshetty.vs@gmail.com>
2019-12-02 15:29:37 +0530

    Completed creating a basic User model(w secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 	
+	validates :password, length: {minimum: 8}, presence: true
 	
 	has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
   
   def setup
   	@user = User.new(name: 'Example User', email: 'user@example.com',
-  			password:'foobar', password_confirmation: 'foobar')
+  			password:'foobar12', password_confirmation: 'foobar12')
   end
   
   test 'should be valid' do 
@@ -64,5 +64,16 @@ class UserTest < ActiveSupport::TestCase
   	@user.email = mixed_case_email
   	@user.save
   	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
+  
+  test 'password should have a minimum length' do
+  	@user.password = @user.password_confirmation = 'a'*3
+  	assert_not @user.valid?
+  end
+  
+  test 'password should be present(nonblank)' do 
+  	@user.password = @user.password_confirmation = ' ' *6
+  	assert_not @user.valid?
   end
 end
