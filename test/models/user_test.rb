80e381f7edeb7914e19983a8bc2f09f488b5aadc
Vedant <vshetty.vs@gmail.com>
2019-12-02 14:54:06 +0530

    Added a uniquness constraint to email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,5 +52,16 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test 'email should be unique' do 
+  	duplicate_user = @user.dup
+  	duplicate_user.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate_user.valid?
+  end
+  
+  test 'email addresses should be in lowercase' do 
+  	mixed_case_email = 'Foo@ExAMPle.CoM'
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
