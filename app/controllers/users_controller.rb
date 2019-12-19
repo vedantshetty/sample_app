85c8fab7dca352f628beb42bc534b79925f4a6e5
Vedant <vshetty.vs@gmail.com>
2019-12-19 07:59:21 +0530

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    @blohh = Time.now
   end
+
+  def create
+    @user = User.new(whitelisted_user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      puts @user.errors.messages
+      @blohh = Time.now
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def whitelisted_user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
